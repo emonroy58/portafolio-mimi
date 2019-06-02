cde027cb3d2f7e53e9e35ee11e4f768f6b3c7c8e
emonroy58 <emgmonroy@gmail.com>
2019-06-02 11:56:25 -0500

    Updates

---
+++ precache-manifest.a6855f7f50be0499716f9de6e6da0e48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828166543143570155bf54feae0c21af",
+    "url": "/portafolio-mimi/index.html"
+  },
+  {
+    "revision": "25c4e1b6a1c9348732b7",
+    "url": "/portafolio-mimi/static/css/2.08c8ee57.chunk.css"
+  },
+  {
+    "revision": "1c5081ac60cc55983a49",
+    "url": "/portafolio-mimi/static/css/main.fe8360fa.chunk.css"
+  },
+  {
+    "revision": "25c4e1b6a1c9348732b7",
+    "url": "/portafolio-mimi/static/js/2.55cff7d9.chunk.js"
+  },
+  {
+    "revision": "1c5081ac60cc55983a49",
+    "url": "/portafolio-mimi/static/js/main.122ed847.chunk.js"
+  },
+  {
+    "revision": "e41602659404c5e4d583",
+    "url": "/portafolio-mimi/static/js/runtime~main.57ed97d1.js"
+  },
+  {
+    "revision": "94e47573f58fccc9ae5608c3a4974252",
+    "url": "/portafolio-mimi/static/media/NOMBRE_dos.94e47573.png"
+  }
+]);
