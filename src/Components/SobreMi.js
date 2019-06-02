65e93ec816020055a5b860d48ff0654e1e148946
Ely Monroy <44978313+emonroy58@users.noreply.github.com>
2019-06-01 19:22:44 -0500

    Update SobreMi.js

--- src/Components/SobreMi.js
+++ src/Components/SobreMi.js
@@ -8,33 +8,38 @@ render (){
 
     return(
         <Navigation >   
-       <div class="container">
-        <main class= "container-text">
-            <h1></h1>
-                <p>
-               <div className="title"><h1>SOBRE MI</h1> </div> <br></br>
-                Reconocer la necesidad es la principal condición para el diseño.
-                </p><br></br>
-                <span className="Autor">Charles Eames</span>
-                <p><br></br></p>
-                <p>Por  favor,   llámame   Mimi,  estudié Diseño   y   Comunicación Visual 
-                en la   Facultad de   Artes y Diseño   UNAM,   finalmente el interés hacia 
-                la tecnología me llevo a Laboratoria, lugar donde me gradúe como UX Designer. 
-                Los conocimientos  como  diseñadora  y  UX me llevan a desarrollar propuestas
-                basadas en el usuario, la funcionalidad y elementos visuales con metodologías 
-                ágiles, artista  digital  e  ilustradora,  que está lista para hacer miles de 
-                diseños modernos, elegantes.
-                </p>   
-                <p><br></br>
-                Disfruto resolviendo problemas y nunca me rindo en el camino por la solución perfecta.
-                </p>
-        </main>
+        <body>
+        <div class="container">
+          
+            <div className="box-text">
+                           <h3>SOBRE MI</h3><br></br>
+                           <span clasName="cita">
+                            Reconocer  la  necesidad  es la principal condición para el diseño. 
+                           </span><br></br>
+                           <div className="cita-autor">
+                           Charles Eames.    
+                           </div> <br></br>
+                           <div className="description">Por  favor,   llámame   Mimi,  estudié Diseño   y   Comunicación Visual 
+                           en la   Facultad de   Artes y Diseño   UNAM,   finalmente el interés hacia 
+                           la tecnología me llevo a Laboratoria, lugar donde me gradúe como UX Designer. 
+                           Los conocimientos  como  diseñadora  y  UX me llevan a desarrollar propuestas
+                           basadas en el usuario, la funcionalidad y elementos visuales con metodologías 
+                           ágiles, artista  digital  e  ilustradora,  que está lista para hacer miles de 
+                           diseños modernos, elegantes.                          
+                          
+                           </div> <br></br>
+                           <div className="description-child"> Disfruto resolviendo problemas y nunca me rindo en el camino por la solución perfecta.</div>
+       
+             </div>
+           
+             <div className="box">
+                <img src="https://i.ibb.co/fnRRW9y/FOTO-SOBRE-MI.png" alt="Image-About" className="img-about"></img>
+            </div> 
+           
+        </div>    
 
-        <aside className="sidebar">
-            <img src="https://i.ibb.co/fnRRW9y/FOTO-SOBRE-MI.png" alt="Image-About" className="img-about"></img>
-        </aside>
-
-       </div>    
+        </body>
+      
      </Navigation>     
     )
  }
